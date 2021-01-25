2a5ff85433133a2c5aae1b092a3069c4240e74f1
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2021-01-25 19:58:39 -0300

    serve assets on /assets

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/assets", express.static("assets"));
 app.use(router);
 
 export default app;
