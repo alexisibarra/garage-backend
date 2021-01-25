a3479890288b8e0180ecbf434ff2eb79502947e5
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2021-01-25 18:14:50 -0300

    use prisma

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config'
-import cors from 'cors'
-import express from 'express'
-import router from './routes'
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import router from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
 
-export default app
+export default app;
