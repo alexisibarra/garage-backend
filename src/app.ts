89be8c72c47f1d4a56db7cdd4507b647fa1f60c3
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2021-01-27 18:35:22 -0300

    apply cache middleware to assets endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
-import cors from "cors";
-import express from "express";
 import router from "./routes";
+import { assetsEndpointValidations } from "./Validators/assetsEndpointValidations";
 import { assetsResizer } from "./Middlewares/assetsResizer";
 import { checkFileExists } from "./Middlewares/checkFileExists";
-import { assetsEndpointValidations } from "./Validators/assetsEndpointValidations";
+import { cache } from "./Middlewares/cache";
 
 const app = express();
+
+const assetsCacheSeconds = Number(process.env.ASSETS_CACHE_SECONDS) || 60;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/assets", checkFileExists, assetsEndpointValidations, assetsResizer);
+app.use(
+  "/assets",
+  checkFileExists,
+  cache(assetsCacheSeconds),
+  assetsEndpointValidations,
+  assetsResizer
+);
 app.use("/assets", express.static("assets"));
 
 app.use(router);
